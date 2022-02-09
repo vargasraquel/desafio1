cb6ea244bbfa9fed642d8ec2c15c83479ac76656
vargasraquel <raquelvargas@gmail.com>
2022-02-09 19:54:12 -0300

    Criando o projeto e commit inicial.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import './styles.css'
 
 function App() {
+  const [ personagens, setPersonagem ] = useState([])
+  const [ busca, setBusca ] = useState('')
+  const [ filtro, setFiltro ] = useState([])
+
+  useEffect(() => {
+    fetch('http://www.irmaodojorel.com/api/Personagem')
+      .then(response =>response.json())
+      .then(data => setPersonagem(data))
+  },[])
+
+  useEffect(() => {
+    setFiltro(
+      personagens.filter(personagem => {
+        return personagem.nome.includes(busca)
+      })
+    )
+  },[ busca, personagens ])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <input 
+        placeholder="Digite o nome do personagem"
+        onChange={e => {setBusca(e.target.value)}}
+      />
+      <div className="cards">
+        {filtro.map(personagem => (
+          <div className="card" key={personagem.id}>
+            <p>{personagem.nome}</p>
+            <img src={personagem.imagemUrl} alt={personagem.nome}/>
+          </div>
+        ))}
+       
+      </div>
     </div>
   );
 }
